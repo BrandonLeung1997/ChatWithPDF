b9e893844e0ae4b0c8116bfe634a40ffbe1e8766
Brandon Leung <brandon.leung@shuion.com.hk>
2024-01-23 17:36:33 +0800

    update

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello World")
+import debugpy
+import streamlit as st
+debugpy.listen(("0.0.0.0", 5678))
 
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your PDFs:")
+
+    with st.sidebar:
+        st.subheader("Your documents:")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
 
 if __name__ == '__main__':
     main()
